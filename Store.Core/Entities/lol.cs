c2fab6cb2b14f35262471bd92992dec9cff7183a
Saikoo <hp@DESKTOP-VBLFIT8>
2024-04-08 17:37:19 +0200

    uppp

---
+++ Store.Core/Entities/lol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Core.Entities
+{
+    internal class lol : BaseEntity
+    {
+        public string  ll { get; set; }
+    }
+}
